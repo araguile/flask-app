9e1443e6f1893c89626c39a4dd55e694b7922aae
araguile <118584365+araguile@users.noreply.github.com>
2023-10-11 16:57:28 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,13 +1,22 @@
-from flask import Flask, jsonify
-import os
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def index():
-    return "Hola mundo"
+    return render_template('index.html')
 
+@app.route('/formularios')
+def formularios():
+    return render_template('formularios.html')
 
-if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+@app.route('/tablas')
+def tablas():
+    return render_template('tablas.html')
+
+@app.route('/tarjetas')
+def tarjetas():
+    return render_template('tarjetas.html')
+
+if __name__ =='__main__':
+	app.run(debug=True,port=5000)
